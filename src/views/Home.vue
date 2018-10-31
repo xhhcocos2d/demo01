cc7498af032716c09604370bb21dd7fdd6cc0a94
李世明 <lsm520wlh@gmail.com>
2018-10-31 17:20:04 +0800

    1.将html标签的lang属性设置为en： <html lang="en"> 2.将chrome的翻译功能打开将英文翻译成中文 3.翻译完成后修改data里面的数据视图就不再更新

---
+++ src/views/Home.vue
@@ -0,0 +1,29 @@
+<template>
+	<div class="home">
+		<!--<img alt="Vue logo" src="../assets/logo.png">-->
+		<p>复现chrome翻译成中文视图不更新：{{test}}</p>
+		<!--<HelloWorld msg="Welcome to Your Vue.js App"/>-->
+	</div>
+</template>
+
+<script>
+  // @ is an alias to /src
+  // import HelloWorld from '@/components/HelloWorld.vue'
+
+  export default {
+    name: 'home',
+    data() {
+      return {
+        test: 0
+      }
+    },
+    components: {
+      // HelloWorld
+    },
+    mounted() {
+      setInterval(() => {
+        this.test += 1
+      }, 1000)
+    }
+  }
+</script>
